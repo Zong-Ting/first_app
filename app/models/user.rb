535e131bfe3444a8da31d24845f403c43a0f8911
CZT <k7276832834@gmail.com>
2016-07-22 16:19:25 +0800

    sec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  before_save {self.email=email.downcase}
+  has_secure_password
+  validates :email, format: { with: /\A[^@]+@[^@]+\z/}, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: 30}
 end
