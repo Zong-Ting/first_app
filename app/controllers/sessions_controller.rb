a41e6a9efe4e124de67e393c3eeb761b3a09de11
CZT <k7276832834@gmail.com>
2016-07-26 10:15:28 +0800

    7/26-1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 	if @user && @user.authenticate(params[:session][:password]) #密碼比對
 	  sign_in(@user)
 	  #session[:session_token] = @user.session_token #記錄登入者session
-	  flash[:success] = "歡迎! , #{@user.name}" #flash訊息
+	  flash[:success] = "歡迎回來~! , #{@user.name}" #flash訊息
 	  redirect_to @user #轉跳頁面
 	else
 	  flash.now[:error] = "Email或密碼有誤"
